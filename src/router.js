ba3768ca76aa5615418c3ff660f5f8c4e525e01d
alexstar2008 <alexstar2008@ukr.net>
2018-05-07 02:26:33 +0300

    App fix

--- src/router.js
+++ src/router.js
@@ -3,5 +3,9 @@ const router = require('express').Router();
 const { connector } = require('./bot');
 
 router.post('/messages', connector.listen());
+router.get('/', function (req, res) {
+    res.send('All is ok!');
+});
 
 module.exports = router;
+
